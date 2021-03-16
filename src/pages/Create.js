dd759b60f27bc43947267ab5defb71410efa7dad
MohiuddinRabby <rabbymohiuddin@gmail.com>
2021-03-16 14:59:40 +0600

    typography example added

---
+++ src/pages/Create.js
@@ -0,0 +1,30 @@
+import { Typography } from "@material-ui/core";
+import React from "react";
+
+const Create = () => {
+  return (
+    <>
+      <Typography
+        variant="h4"
+        component="h2"
+        align="center"
+        color="primary"
+        gutterBottom
+      >
+        Create Page
+      </Typography>
+      <Typography
+        noWrap
+        variant="subtitle1"
+        align="center"
+        gutterBottom
+        color="textSecondary"
+      >
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime dolorem
+        quos hic ex veniam iure officia voluptate modi. Earum, ipsam!
+      </Typography>
+    </>
+  );
+};
+
+export default Create;
---
+++ src/pages/Notes.js
@@ -0,0 +1,12 @@
+import { Typography } from "@material-ui/core";
+import React from "react";
+
+const Notes = () => {
+  return (
+    <>
+      <Typography>Notes page</Typography>
+    </>
+  );
+};
+
+export default Notes;
