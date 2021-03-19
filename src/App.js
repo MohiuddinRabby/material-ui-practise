84a1b9c8724420130bce3a3c51f7ebec3abb1299
MohiuddinRabby <rabbymohiuddin@gmail.com>
2021-03-20 00:10:31 +0600

    theme context created in context folder

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
-import { createMuiTheme, ThemeProvider } from "@material-ui/core";
+import { ThemeProvider } from "@material-ui/core";
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { theme } from "./context/MuiThemeProvider/theme";
 import Create from "./pages/Create";
 import Notes from "./pages/Notes";
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: "#fefefe",
-    },
-    secondary: {
-      main: "#e91e63",
-    },
-  },
-});
 const App = () => {
   return (
     <div>
---
+++ src/context/MuiThemeProvider/theme.js
@@ -0,0 +1,12 @@
+import { createMuiTheme } from "@material-ui/core";
+
+export const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#fefefe",
+    },
+    secondary: {
+      main: "#2196f3",
+    },
+  },
+});
