5b3480423573e9c49c4ed0e4c9ee5bab70bbf04d
MohiuddinRabby <rabbymohiuddin@gmail.com>
2021-03-20 19:48:07 +0600

    formik used in Create.js

--- src/context/MuiThemeProvider/theme.js
+++ src/context/MuiThemeProvider/theme.js
@@ -1,12 +1,11 @@
 import { createMuiTheme } from "@material-ui/core";
+import { purple } from "@material-ui/core/colors";
 
 export const theme = createMuiTheme({
   palette: {
     primary: {
       main: "#fefefe",
     },
-    secondary: {
-      main: "#2196f3",
-    },
+    secondary: purple,
   },
 });
--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,39 +1,88 @@
 import React from "react";
-import { Button, Container, Typography } from "@material-ui/core";
-import AcUnitIcon from "@material-ui/icons/AcUnit";
-import AccessAlarmIcon from "@material-ui/icons/AccessAlarm";
-import { makeStyles } from "@material-ui/core";
-const useStyles = makeStyles({});
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import * as Yup from "yup";
+import { Button, Container, makeStyles, TextField } from "@material-ui/core";
+const validationSchema = Yup.object().shape({
+  title: Yup.string().required("Required"),
+  details: Yup.string().required("Required"),
+});
+const useStyles = makeStyles({
+  tField: {
+    marginTop: "10px",
+    marginBottom: "10px",
+  },
+  btn: {
+    marginTop: "20px",
+  },
+  requiredMsgColor: {
+    color: "#d50000",
+  },
+});
+const initialValues = {
+  title: "",
+  details: "",
+};
+
 const Create = () => {
   const classes = useStyles();
   return (
-    <Container>
-      <Typography variant="h4" component="h2" color="primary" gutterBottom>
-        Create Page
-      </Typography>
-      <Typography
-        className={classes.paraText}
-        variant="subtitle1"
-        // align="center"
-        gutterBottom
-        color="textSecondary"
-      >
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
-        recusandae tempora nobis delectus veniam natus provident. Eos
-        voluptatibus animi quos quaerat laborum fugit praesentium. Culpa
-        incidunt architecto corrupti eos adipisci.
-      </Typography>
-      <Button
-        className={classes.btn}
-        color="secondary"
-        variant="contained"
-        size="small"
-        startIcon={<AcUnitIcon />}
-        endIcon={<AccessAlarmIcon />}
-      >
-        Submit
-      </Button>
-    </Container>
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={(values, { setSubmitting, resetForm }) => {
+        console.log(values);
+        resetForm();
+      }}
+    >
+      <Container>
+        <Form>
+          <div>
+            <Field
+              name="title"
+              type="text"
+              as={TextField}
+              label="Title"
+              color="secondary"
+              variant="outlined"
+              className={classes.tField}
+              multiline
+              fullWidth
+            />
+          </div>
+          <div className={classes.requiredMsgColor}>
+            <ErrorMessage name="title" />
+          </div>
+          <div>
+            <Field
+              name="details"
+              type="text"
+              as={TextField}
+              label="Details"
+              color="secondary"
+              variant="outlined"
+              className={classes.tField}
+              multiline
+              rows={4}
+              fullWidth
+            />
+          </div>
+          <div className={classes.requiredMsgColor}>
+            <ErrorMessage name="details" />
+          </div>
+          <div>
+            <Button
+              type="submit"
+              color="secondary"
+              variant="contained"
+              size="small"
+              className={classes.btn}
+            >
+              Submit
+            </Button>
+          </div>
+        </Form>
+      </Container>
+    </Formik>
   );
 };
 
