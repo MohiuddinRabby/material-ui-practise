2a3963f8c3ddb49e1f5b47cc6a11bc5f7e169a3e
MohiuddinRabby <rabbymohiuddin@gmail.com>
2021-03-25 00:22:36 +0600

    card added

---
+++ src/pages/NoteCard.js
@@ -0,0 +1,32 @@
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  IconButton,
+  Typography,
+} from "@material-ui/core";
+import React from "react";
+import DeleteIcon from "@material-ui/icons/Delete";
+const NoteCard = ({ note }) => {
+  return (
+    <Card elevation={3}>
+      <CardHeader
+        action={
+          <IconButton aria-label="settings">
+            <DeleteIcon />
+          </IconButton>
+        }
+        title={note?.title}
+      />
+      <CardMedia image="/static/images/cards/paella.jpg" title="Paella dish" />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          {note?.details}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default NoteCard;
--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,6 +1,7 @@
-import { Container, Grid, Paper } from "@material-ui/core";
+import { Container, Grid } from "@material-ui/core";
 import React, { useEffect, useState } from "react";
 import { getNotes } from "../Api/Api";
+import NoteCard from "./NoteCard";
 const Notes = () => {
   const [notes, setNotes] = useState([]);
   useEffect(() => {
@@ -8,10 +9,10 @@ const Notes = () => {
   }, []);
   return (
     <Container>
-      <Grid container>
+      <Grid container spacing={3}>
         {notes?.map((note) => (
           <Grid key={note?.id} item xs={12} sm={6} md={4} lg={4}>
-            <Paper>{note?.title}</Paper>
+            <NoteCard note={note} />
           </Grid>
         ))}
       </Grid>
